aedac113c2ebcee2e7dfbf0b2188f7c0e0f488aa
Sean O'Connor <so@seano.dev>
2024-12-09 18:27:20 -0700

    Init Spring Boot application

---
+++ api/src/main/java/dev/seano/bookwyrm/BookwyrmApplication.java
@@ -0,0 +1,12 @@
+package dev.seano.bookwyrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookwyrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookwyrmApplication.class, args);
+    }
+}
